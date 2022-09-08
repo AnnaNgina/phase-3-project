a849436b080e650cda662f6dbb269321140e4df9
annastacia <annastaciangina96@gmail.com>
2022-09-09 01:19:36 +0300

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function Header() {
+    return ( 
+      
+      <div className="header"> 
+      <pre>
+      <p> The system is approved by government for crime analytics.<br></br>
+        You can add crime in your village and also add comment on the services offerred by the authourity. <br></br>
+        Also you can comment on that system.</p>
+        </pre>
+
+             </div>
+    
+ );
+
+}
+ export default Header;
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+// import "./Navbar.css";
+
+function Navbar() {
+    return ( 
+      
+      <div className="header">
+         <h1>Crime Report System</h1>
+         
+             <div id="links">
+             <a href="#home">Home</a> 
+            <a href="#reportcase">Report Case</a>
+            <a href="#comment">Comment</a>
+             
+              
+               
+             </div>
+        </div>
+ );
+
+}
+ export default Navbar;
