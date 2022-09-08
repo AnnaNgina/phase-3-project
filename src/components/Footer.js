aa9cfbbbec239df435f8d2009940c1f7fb817c9d
annastacia <annastaciangina96@gmail.com>
2022-09-09 01:35:57 +0300

    commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+function Footer() {
+    return ( 
+      
+      <div className="footer"> 
+      <div id ="links">
+      <p>Created by @Annastacia for crime report</p>
+
+             </div>
+             </div>
+    
+ );
+
+}
+ export default Footer;
